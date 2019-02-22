4dca78dbe8f4e82b38f18f48cce5464f749b9ca1
Everton Monte da Silva <everton0189@gmail.com>
2019-02-21 23:51:21 -0300

    Adicionado logica para conectar ao Remote Config Service

--- src/main/java/com/rollingstone/RollingstoneEcommerceProductApiApplication.java
+++ src/main/java/com/rollingstone/RollingstoneEcommerceProductApiApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class RollingstoneEcommerceProductApiApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(RollingstoneEcommerceProductApiApplication.class, args);
+        SpringApplication.run(RollingstoneEcommerceProductApiApplication.class, args);
     }
 }
